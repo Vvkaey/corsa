f7ba00c8065db8b8023d79cc87cfef152200d764
apnwr <ayushb147@outlook.com>
2025-07-23 01:54:36 +0530

    [FIX] seo fixes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,7 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1.0,
+      priority: 0.99,
     },
     {
       url: `${baseUrl}/pricing`,
